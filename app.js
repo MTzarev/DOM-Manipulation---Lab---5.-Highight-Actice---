dc53e83184ea8b0e0adb528250d5c325a4f1757c
Miroslav Tsarev <mtzarev@gmail.com>
2024-05-16 23:40:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 function focused() {
     document.querySelectorAll(`input`).forEach(element => {
-        element.addEventListener(`focus`, onFocus)
+        element.addEventListener(`focus`, onFocus);
         element.addEventListener(`blur`, onBlur);
     });
 function onFocus(ev){
-ev.target.parentNode.className = `focus`;
+        ev.target.parentNode.className = `focus`;
 }
 function onBlur(ev){
-ev.target.parentNode.className = ``;
- //console.log(`blured`)   
+        ev.target.parentNode.className = ``;   
 }
 }
